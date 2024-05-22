01861c6c090d7ec7cdb2217bd250df78b6d22a2e
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-05-22 22:04:02 +0400

    Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,27 +1,55 @@
-// import NewsList from "@/components/NewsList";
-import { getAvailableNewsYears } from "@/lib/news";
+import NewsList from "@/components/NewsList";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
-// import { getNewsForYear } from "@/lib/news";
+import { getNewsForYear } from "@/lib/news";
 
 export default function FilterdNewsPage({ params }) {
   const filter = params.filter;
-  console.log(filter);
-  const links = getAvailableNewsYears();
-  // const news = getNewsForYear(newsYear);
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
 
-  // return <NewsList news={news} />;
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>No news found for the selected period</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
   );
 }
