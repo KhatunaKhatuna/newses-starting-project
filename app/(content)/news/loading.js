2ceb992f0aee92efa49c5870dedb388d6f6b35c4
KhatunaKhatuna <kh.chitishvili@gmail.com>
2024-05-26 21:43:30 +0400

    Showing a Loading Fallback

---
+++ app/(content)/news/loading.js
@@ -0,0 +1,3 @@
+export default function Newsloading() {
+  return <p>Loading...</p>;
+}
--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -2,7 +2,7 @@ import NewsList from "@/components/NewsList";
 import { getAllNews } from "@/lib/news";
 
 export default async function NewsPage() {
-  const news = getAllNews();
+  const news = await getAllNews();
 
   return (
     <>
